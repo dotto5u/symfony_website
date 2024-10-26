e91cedba6bd02d9ac22b8e3f4001cb894dead3c3
dotto5u <matis.dotto7@etu.univ-lorraine.fr>
2024-10-26 20:05:35 +0200

    Add working login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -13,31 +13,18 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        $lastUsername = $authenticationUtils->getLastUsername();
+        $form = $this->createForm(LoginFormType::class);
+        $form->get('email')->setData($authenticationUtils->getLastUsername());
         $error = $authenticationUtils->getLastAuthenticationError();
 
-        $form = $this->createForm(LoginFormType::class, [
-            'username' => $lastUsername
-        ]);
-
-        if ($this->getUser()) {
-            $user = $this->getUser();
-            if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
-                return $this->redirectToRoute('app_admin');
-            }
-    
-            return $this->redirectToRoute('app_home');
-        }
-        
         return $this->render('login.html.twig', [
-            'last_username' => $lastUsername,
+            'form' => $form,
             'error' => $error,
-            'form' => $form->createView()
         ]);
     }
 
     #[Route('/logout', name: 'app_logout')]
     public function logout(): void {
-
+        // logout géré par le security.yaml
     }
 }
--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -8,50 +8,48 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LoginFormType extends AbstractType
 {
+    private TranslatorInterface $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', EmailType::class, [
-                'label' => 'Email',
+            ->add('email', EmailType::class, [
+                'label' => $this->translator->trans('login.email.label'),
                 'attr' => [
-                    'placeholder' => 'Entrez votre email'
-                ],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez entrer votre adresse email',
-                    ]),
-                    new Email([
-                        'message' => 'Veuillez entrer une adresse email valide',
-                    ]),
-                    new Length([
-                        'max' => 25,
-                        'maxMessage' => 'L\'email ne doit pas dépasser {{ limit }} caractères',
-                    ]),
+                    'placeholder' => $this->translator->trans('login.email.placeholder'),
                 ],
             ])
             ->add('password', PasswordType::class, [
-                'label' => 'Mot de passe',
+                'label' => $this->translator->trans('login.password.label'),
                 'attr' => [
-                    'placeholder' => 'Entrez votre mot de passe'
-                ],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Veuillez entrer votre mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 5,
-                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
-                    ]),
+                    'placeholder' => $this->translator->trans('login.password.placeholder'),
                 ],
             ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Se connecter',
+                'label' => $this->translator->trans('login.submit.label'),
             ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => true,
+            'csrf_field_name' => '_csrf_token',
+            'csrf_token_id' => 'authenticate',
+        ]);
     }
 }
